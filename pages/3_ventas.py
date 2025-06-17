a3ed08c4870506e2ed4e548ef072da6d0d4740c2
jufragarcaCUN <juan_garnicac@cun.edu.co>
2025-06-17 14:20:10 -0500

    Update 3_ventas.py

--- pages/3_ventas.py
+++ pages/3_ventas.py
@@ -20,7 +20,7 @@ st.set_page_config(layout="wide")
 # Definir la ruta base donde se encuentra el archivo de datos.
 carpeta_base = Path(__file__).parent.parent / "data"
 # Construir la ruta completa al archivo Excel principal.
-archivo_principal = carpeta_base / "ventasBi.xlsx"
+archivo_principal = carpeta_base / "ventasasdfasdfsadfBi.xlsx"
 
 # Cargar el archivo Excel en un DataFrame de pandas.
 try:

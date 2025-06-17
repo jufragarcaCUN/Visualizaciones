13f7c38792c0d4769fda7a0fded1b93257d90b5d
jufragarcaCUN <juan_garnicac@cun.edu.co>
2025-06-17 12:21:47 -0500

    Update 3_rematricula.py

--- pages/3_rematricula.py
+++ pages/3_rematricula.py
@@ -20,7 +20,7 @@ st.set_page_config(layout="wide")
 # Definir la ruta base donde se encuentra el archivo de datos.
 carpeta_base = Path(__file__).parent.parent / "data"
 # Construir la ruta completa al archivo Excel principal.
-archivo_principal = carpeta_base / "reporte_completo_con_celular_ventas_REMATRICULA16.xlsx"
+archivo_principal = carpeta_base / "reporte_completo_con_celular_Rematricula_BI.xlsx"
 
 # Cargar el archivo Excel en un DataFrame de pandas.
 try:
